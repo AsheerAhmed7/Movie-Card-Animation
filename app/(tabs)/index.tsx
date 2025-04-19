6028bd54f5b346ed879b76b249caa03aadf67f14
AsheerAhmed7 <assheer7@gmail.com>
2025-04-19 14:46:52 +0500

    Adding Project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,175 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  interpolateColor,
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const { height, width } = Dimensions.get("screen");
+type backDrop = {
+  title: { poster: ImageSourcePropType };
+  index: number;
+  color: any;
+};
+const BackDropImage: React.FC<backDrop> = ({ index, title, color }) => {
+  const styleZ = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      color.value,
+      [index - 1, index, index + 1],
+      [0, 0.5, 0]
+    );
+    return {
+      opacity,
+    };
+  });
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <Animated.Image
+      source={title.poster}
+      key={index.toString()}
+      style={[
+        StyleSheet.absoluteFillObject,
+        { width, height, resizeMode: "cover" },
+        styleZ,
+      ]}
+    />
+  );
+};
+export default function HomeScreen() {
+  type PosterType = {
+    poster: ImageSourcePropType;
+  };
+
+  const hollywoodMoviePosters: PosterType[] = [
+    {
+      poster: require("../../assets/images/titanic.jpg"),
+    },
+    {
+      poster: require("../../assets/images/marvel.jpg"),
+    },
+    {
+      poster: require("../../assets/images/batman.jpg"),
+    },
+    {
+      poster: require("../../assets/images/spiderman.jpg"),
+    },
+  ];
+
+  const translate_X = useSharedValue(0);
+  const scrollEvent = useAnimatedScrollHandler((event) => {
+    translate_X.value = event.contentOffset.x;
+  });
+
+  const color = useDerivedValue(() => {
+    console.log(translate_X.value);
+
+    if (translate_X.value >= 180 && translate_X.value < 380) {
+      return withTiming(1);
+    } else if (translate_X.value >= 380 && translate_X.value < 580) {
+      return withTiming(2);
+    } else if (translate_X.value >= 580) {
+      return withTiming(3);
+    }
+    return withTiming(0);
+  }, [translate_X.value]);
+
+  return (
+    <Animated.View style={[{ flex: 1 }]}>
+      <View style={[StyleSheet.absoluteFillObject]}>
+        {hollywoodMoviePosters.map((title: any, index) => {
+          return <BackDropImage index={index} title={title} color={color} />;
+        })}
+      </View>
+      <Animated.ScrollView
+        horizontal
+        scrollEnabled={true}
+        onScroll={scrollEvent}
+        scrollEventThrottle={16}
+        contentContainerStyle={styles.container}
+      >
+        {hollywoodMoviePosters.map((title: any, index) => {
+          return <Card title={title} index={index} translate_X={translate_X} />;
+        })}
+      </Animated.ScrollView>
+    </Animated.View>
   );
 }
+import { ImageSourcePropType } from "react-native";
+
+type CardProps = {
+  title: { poster: ImageSourcePropType };
+  index: number;
+  translate_X: Animated.SharedValue<number>;
+};
+
+const Card: React.FC<CardProps> = ({ index, title, translate_X }) => {
+  const input = [(index - 1) * 200, index * 200, (index + 1) * 200];
+  const cardStyle = useAnimatedStyle(() => {
+    const scale = interpolate(
+      translate_X.value,
+      input,
+      [-40, 1, 40],
+      Extrapolation.CLAMP
+    );
+    return {
+      transform: [{ rotateY: `${scale}deg` }],
+    };
+  });
+  return (
+    <View
+      key={index.toString()}
+      style={{ marginLeft: width * 0.001, marginRight: width * 0.1 }}
+    >
+      <Animated.View style={[styles.card, cardStyle]}>
+        <Image source={title.poster} style={styles.image} />
+      </Animated.View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  card: {
+    height: 300,
+    width: 200,
+
+    margin: 8,
+    marginRight: 0,
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+    elevation: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  image: {
+    height: 300,
+    width: 200,
+    resizeMode: "contain",
   },
 });
